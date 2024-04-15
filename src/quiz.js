f6d63a2b448f3093faedf9c49ade70805f716d34
Rafael Guerra <rafa99gm@hotmail.com>
2024-04-15 18:17:24 +0100

    solved

--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,45 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        return this.currentQuestionIndex ++;
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions() {
+        for (let i = 0; i < this.questions.length; i++) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+        return this.questions;
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        const question = this.getQuestion()
 
-    // 6. hasEnded()
+        if (answer === question.answer) {
+            return this.correctAnswers ++;
+        }
+    }
+
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        } else if (this.currentQuestionIndex === this.questions.length) {
+            return true;
+        }
+    }
 }
+
+
